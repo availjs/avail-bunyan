b6c0864a2d4228dd824635f1b80e4028c1b3aa86
Justin Beaudry <beaudry.justin@gmail.com>
2018-08-12 23:00:16 -0700

    adds, addLogger method so withBunyan can be used in an extend and still be configured

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ const WithBunyan = require('./src/WithBunyan');
 const AddStandardLoggers = require('./src/AddStandardLoggers');
 
 module.exports = WithBunyan;
-module.exports.AddStandardLoggers = AddStandardLoggers;
+exports.AddStandardLoggers = AddStandardLoggers;
